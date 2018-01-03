66a832b912ebf31ef58b7201e49b8a98117d8dc3
vemperala <vemperalaonpur@gmail.com>
2018-01-03 11:50:48 -0500

    pdf doc complete

--- src/main/java/com/cassandra/controller/PdfDocController.java
+++ src/main/java/com/cassandra/controller/PdfDocController.java
@@ -1,10 +1,25 @@
 package com.cassandra.controller;
 
+import com.cassandra.service.PdfDocService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by akhilesh on 1/3/18.
  */
-@Controller
+@RestController
+@RequestMapping("/pdfdoc")
 public class PdfDocController {
+
+    @Autowired
+    private PdfDocService pdfDocService;
+
+    @RequestMapping("/")
+    public void downloadFile(HttpServletResponse response){
+        pdfDocService.downloadPdf(response);
+    }
 }
--- src/main/java/com/cassandra/service/PdfDocService.java
+++ src/main/java/com/cassandra/service/PdfDocService.java
@@ -1,6 +1,19 @@
 package com.cassandra.service;
 
+import com.cassandra.model.PdfDoc;
+import com.cassandra.repository.PdfDocRepo;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+import sun.misc.IOUtils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * Created by akhilesh on 1/3/18.
@@ -8,4 +21,26 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PdfDocService {
+
+    @Autowired
+    private PdfDocRepo pdfDocRepo;
+
+
+    public void downloadPdf(HttpServletResponse response){
+
+        List<PdfDoc> pdfDoc = (List)pdfDocRepo.findAll();
+
+        pdfDoc.stream().forEach(e -> {
+            try{
+                InputStream is = new ByteArrayInputStream(e.getDocument().array());
+
+                StreamUtils.copy(is, response.getOutputStream());
+
+                response.flushBuffer();
+                response.setContentType("application/pdf");
+            }catch (IOException ex ){
+                ex.printStackTrace();
+            }
+        });
+    }
 }
